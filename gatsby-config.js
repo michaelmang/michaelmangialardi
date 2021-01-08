1c2dd191cece850525cf813f787a02be17d1e427
Mike Mangialardi <mike.mangialardi@rackspace.com>
2021-01-07 20:40:56 -0500

    Initialize sourcing from graphcms

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,13 @@ const path = require("path")
 
 const tailwindConfig = require("./tailwind.config.js")
 
+const dotenv = require('dotenv')
+console.log({ dotenv })
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+console.log(process.env);
+
 module.exports = {
   /* Your site config here */
   plugins: [
@@ -33,5 +40,14 @@ module.exports = {
     },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-graphcms',
+        options: {
+          // Your GraphCMS API endpoint. Available from your project settings.
+          endpoint: process.env.GRAPHCMS_ENDPOINT,
+          // A PAT (Permanent Auth Token) for your project. Required if your project is not available publicly, or you want to scope access to a specific content stage (i.e. draft content).
+          token: process.env.GRAPHCMS_TOKEN,
+        },
+    },
   ],
 }
