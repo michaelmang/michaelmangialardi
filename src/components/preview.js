e4da45704fc02e720a2ca58833ef668eac2d80ff
Mike Mangialardi <mike.mangialardi@rackspace.com>
2021-01-11 12:59:11 -0500

    Open post via preview title

--- src/components/preview.js
+++ src/components/preview.js
@@ -1,19 +1,21 @@
 import { Link } from 'gatsby'
 import { useHover } from 'react-use'
 
-export default function Preview({ children, className, cta: defaultCta = "Read more", slug = "/", subtitle, title: defaultTitle }) {
+export default function Preview({ children, className, cta: defaultCta = "Read more", slug = "/", subtitle, title: defaultTitle, ...rest }) {
   function title(hovered) {
     return (
-      <div className={`cursor-pointer text-lg w-max ${hovered ? 'text-cta' : 'text-dark dark:text-light'} font-bold`}>{defaultTitle}</div>
+      <Link to={slug}>
+        <h3 className={`cursor-pointer text-lg w-max ${hovered ? 'text-cta' : 'text-dark dark:text-light'} font-bold`}>{defaultTitle}</h3>
+      </Link>
     )
   }
 
   const [hoverableTitle, isTitleHovered] = useHover(title);
 
   return (
-    <div className={`flex flex-col mb-6 ${className}`}>
+    <div {...rest} className={`flex flex-col mb-6 ${className}`}>
       {hoverableTitle}
-      {subtitle && <div className="text-dark dark:text-light opacity-75 text-base font-bold">{subtitle}</div>}
+      {subtitle && <h4 className="text-dark dark:text-light opacity-75 text-base font-bold">{subtitle}</h4>}
       <div className="my-4 text-base text-dark dark:text-light text-justify w-3/4">{children}</div>
       <Link className={`cursor-pointer text-base hover:text-cta ${isTitleHovered ? 'text-cta' : 'text-dark dark:text-light'} font-bold`} to={slug}>{defaultCta}</Link>
     </div>
