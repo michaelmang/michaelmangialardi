9af27fca4238dfddfbe3d87d64665f9615510936
Mike Mangialardi <mike.mangialardi@rackspace.com>
2021-01-11 10:46:21 -0500

    Extract theme toggle

---
+++ src/components/theme-toggle.js
@@ -0,0 +1,39 @@
+import { Fragment } from "react"
+import { animated, useSpring } from "react-spring"
+import { useToggle } from "react-use"
+
+import { state } from "./provider"
+import CoinBlock from "../images/coin_block.svg"
+import ReactMushroom from "../images/react_mushroom.svg"
+
+export default function ThemeToggle() {
+  const [smashed, toggleSmashed] = useToggle(false)
+
+  const moon = { color: "#0A0C10", y: 0 }
+  const sun = { color: "#FFE773", y: 0 }
+  const spring = useSpring({
+    from: smashed ? sun : moon,
+    to: smashed ? moon : sun,
+  })
+  const AnimatedReactMushroom = animated(ReactMushroom)
+
+  return (
+    <state.Consumer>
+      {context => (
+        <Fragment>
+          <AnimatedReactMushroom
+            className={`h-16 w-auto mb-2`}
+            style={spring}
+          />
+          <CoinBlock
+            className={`cursor-pointer h-16 w-auto mb-10 transition-all text-cta delay-75 hover:text-background`}
+            onClick={() => {
+              context.changeTheme()
+              toggleSmashed()
+            }}
+          />
+        </Fragment>
+      )}
+    </state.Consumer>
+  )
+}
