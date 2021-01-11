5f589a88038953ada0adc6772ee2807d361ae96d
Mike Mangialardi <mike.mangialardi@rackspace.com>
2021-01-11 12:59:31 -0500

    Add blog categories

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,24 +1,56 @@
 import { graphql } from "gatsby"
+import startcase from "lodash.startcase"
+import { useState } from "react"
 import { WindupChildren } from "windups"
 
 import Layout from "../components/layout"
+import Pill from "../components/pill"
 import Preview from "../components/preview"
 import ThemeToggle from "../components/theme-toggle"
 
+function uniq(list) {
+  return [...new Set(list)]
+}
+
 export default function Blog({ data }) {
+  const categories = uniq(data.allGraphCmsPost.edges.flatMap(({ node }) => (
+    node.tags.filter(tag => tag !== "blog")
+  )))
+
+  const [filter, setFilter] = useState("all")
+
   return (
     <Layout>
       <div className="h-full w-full flex flex-col">
-        <WindupChildren>
-          <h1 className="mt-8 text-3xl text-dark dark:text-light font-black leading-10">
-            Posts
-          </h1>
-        </WindupChildren>
-        <div className="flex flex-row flex-wrap justify-between w-full mt-6">
-          {data.allGraphCmsPost.edges.map(({ node }) => (
+          {!!categories.length && (
+            <div className="flex flex-col my-6">
+              <h1 className="mb-3 text-3xl text-dark dark:text-light font-black leading-10">
+                Categories
+              </h1>
+              <div className="flex flex-row items-center">
+                {["all", ...categories].map(category => (
+                  <Pill
+                    className={`${filter === category ? "bg-cta" : ""} hover:bg-cta cursor-pointer`}
+                    onClick={() => setFilter(category)}
+                  >
+                    <WindupChildren>
+                      {startcase(category.toUpperCase())}
+                    </WindupChildren>
+                  </Pill>
+                ))}
+              </div>
+            </div>
+          )}
+        <div className="flex flex-row my-6">
+          <div className="text-3xl text-dark dark:text-light font-black leading-10">
+            {startcase(filter.toLowerCase())} Posts
+          </div>
+        </div>
+        <div className="flex flex-row flex-wrap w-full">
+          {data.allGraphCmsPost.edges.filter(({ node }) => filter === "all" || node.tags.includes(filter)).map(({ node }) => (
             <Preview
               key={node.title}
-              className="text-light w-1/3 p-8 rounded-lg shadow-xl border-background border-4"
+              className="text-light w-1/3 p-8 rounded-lg shadow-xl border-background dark:border-accent border-4 mr-6"
               slug={node.slug}
               subtitle={node.subtitle}
               title={node.title}
