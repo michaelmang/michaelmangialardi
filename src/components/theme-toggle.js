ada045817c888190d2ea476bd3f8c37910e34cc6
Mike Mangialardi <mike.mangialardi@rackspace.com>
2021-01-13 10:34:43 -0500

    Persist dark mode state across pages

--- src/components/theme-toggle.js
+++ src/components/theme-toggle.js
@@ -1,19 +1,17 @@
-import { Fragment } from "react"
+import { Fragment, useContext } from "react"
 import { animated, useSpring } from "react-spring"
-import { useToggle } from "react-use"
 
 import { state } from "./provider"
 import CoinBlock from "../images/coin_block.svg"
 import ReactMushroom from "../images/react_mushroom.svg"
 
 export default function ThemeToggle() {
-  const [smashed, toggleSmashed] = useToggle(false)
-
   const moon = { color: "#0A0C10" }
   const sun = { color: "#FFE773" }
+  const { isDark } = useContext(state)
   const spring = useSpring({
-    from: smashed ? sun : moon,
-    to: smashed ? moon : sun,
+    from: isDark ? sun : moon,
+    to: isDark ? moon : sun,
   })
   const AnimatedReactMushroom = animated(ReactMushroom)
 
@@ -29,7 +27,6 @@ export default function ThemeToggle() {
             className={`cursor-pointer h-10 md:h-16 w-auto mb-6 md:mb-10 transition-all text-cta delay-75 hover:text-background`}
             onClick={() => {
               context.changeTheme()
-              toggleSmashed()
             }}
           />
         </Fragment>
