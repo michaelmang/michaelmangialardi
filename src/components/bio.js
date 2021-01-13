f13d18897ef03a47fbc2865bc0e624f3451a675f
Mike Mangialardi <mike.mangialardi@rackspace.com>
2021-01-13 10:35:01 -0500

    Update styles

--- src/components/bio.js
+++ src/components/bio.js
@@ -3,7 +3,7 @@ import Img from "gatsby-image"
 export default function Bio({ children, className, fluid, name }) {
   return (
     <div className={`flex flex-row ${className} justify-center`}>
-      <div className="flex flex-col mr-6 h-32 w-32 overflow-hidden rounded-full border-accent dark:border-background border-4">
+      <div className="flex flex-col mr-6 h-32 w-32 overflow-hidden rounded-full border-background dark:border-background-light border-4">
         <Img fluid={fluid} />
       </div>
       <div className="flex flex-col w-2/3">
