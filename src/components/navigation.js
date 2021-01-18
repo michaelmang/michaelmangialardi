33557b29629a5570ee6ae89d5258439746b2d95a
Mike Mangialardi <mike.mangialardi@rackspace.com>
2021-01-18 14:30:38 -0500

    Fix hamburger menu flash

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -22,40 +22,36 @@ export default function Navigation() {
       </Link>
 
       <div className="flex flex-col justify-center items-end md:flex-row">
-        {width < MD && !expandMenu && <Icon className="text-cta mt-1" icon={faBars} onClick={toggleExpandMenu} />}
-        {width < MD && expandMenu && <Icon className="text-cta mt-1 mb-4" icon={faTimes} onClick={toggleExpandMenu} />}
+        {!expandMenu && <Icon className="md:hidden text-cta mt-1" icon={faBars} onClick={toggleExpandMenu} />}
+        {expandMenu && <Icon className="md:hidden text-cta mt-1 mb-4" icon={faTimes} onClick={toggleExpandMenu} />}
         
-        {(width >= MD || expandMenu) && (
-          <Fragment>
-            <Link
-              className="mr-0 md:mr-6 text-cta font-bold text-sm md:text-lg tracking-wider"
-              to="/blog"
-            >
-              Blog
-            </Link>
+        <Link
+          className={`${expandMenu ? "flex" : "hidden"} md:flex mr-0 md:mr-6 text-cta font-bold text-sm md:text-lg tracking-wider`}
+          to="/blog"
+        >
+          Blog
+        </Link>
 
-            <Link
-              className="mr-0 my-2 md:my-0 md:mr-6 text-cta font-bold text-sm md:text-lg tracking-wider"
-              to="/"
-            >
-              Work
-            </Link>
+        <Link
+          className={`${expandMenu ? "flex" : "hidden"} md:flex mr-0 my-2 md:mr-6 md:my-0 text-cta font-bold text-sm md:text-lg tracking-wider`}
+          to="/"
+        >
+          Work
+        </Link>
 
-            <Link
-              className="mr-0 mb-2 md:mb-0 md:mr-6 text-cta font-bold text-sm md:text-lg tracking-wider"
-              to="/newsletter"
-            >
-              Newsletter
-            </Link>
+        <Link
+          className={`${expandMenu ? "flex" : "hidden"} md:flex mr-0 mb-2 md:mr-6 md:mb-0 text-cta font-bold text-sm md:text-lg tracking-wider`}
+          to="/newsletter"
+        >
+          Newsletter
+        </Link>
 
-            <a
-              className="text-cta font-bold text-sm md:text-lg tracking-wider"
-              href="mailto:mikemangialardi94@gmail.com"
-            >
-              Contact
-            </a>
-          </Fragment>
-        )}
+        <a
+          className={`${expandMenu ? "flex" : "hidden"} md:flex text-cta font-bold text-sm md:text-lg tracking-wider`}
+          href="mailto:mikemangialardi94@gmail.com"
+        >
+          Contact
+        </a>
       </div>
     </nav>
   )
