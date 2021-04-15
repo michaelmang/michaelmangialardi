61c584aed3272ad50a135f010dcd77df4138e8d4
Mike Mangialardi <mikemangialardi94@gmail.com>
2021-04-15 09:55:10 -0400

    Update CTA text

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,23 @@
 import Navigation from "./navigation"
 import Footer from "./footer"
 import { state } from "./provider"
-import { split } from "@apollo/client"
 
 export default function Layout({ children, cta = false }) {
   return (
     <state.Consumer>
       {context => (
         <div className={context.isDark ? "dark" : "light"}>
-          {cta && <a className="flex justify-center p-4 h-30 w-full bg-background text-light text-base md:text-lg" href="https://leanpub.com/designsystemsfordevelopers" data-splitbee-event="Visit ebook landing page">
-            <span className="mr-2">✨</span> Hey! I've organized my thoughts and experiences on design tokens into an ebook! Click here to learn more.<span className="ml-2">✨</span>
-          </a>}
+          {cta && (
+            <a
+              className="flex justify-center p-4 h-30 w-full bg-gradient-to-r from-background-light to-background text-light text-base md:text-lg"
+              href="https://leanpub.com/designsystemsfordevelopers"
+              data-splitbee-event="Visit ebook landing page"
+            >
+              <span className="mr-2">✨</span>My new ebook
+              &nbsp;<em className="italic">Design Systems for Developers</em>&nbsp; is 
+              here! Start reading<span className="ml-2">✨</span>
+            </a>
+          )}
           <div
             className={`font-display min-h-screen w-screen bg-light dark:bg-dark px-8 py-5 md:px-20 md:py-10`}
           >
